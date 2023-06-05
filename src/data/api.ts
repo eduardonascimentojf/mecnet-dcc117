13cc21cb400ae2536a3945db80cb1060718a0673
Eduardo Araujo <eduardoaraujo.jf@gmail.com>
2023-06-05 12:07:18 -0300

    Api

--- src/data/api.ts
+++ src/data/api.ts
@@ -4,5 +4,5 @@ export const api = axios.create({
   baseURL: "https://dummyjson.com/",
 });
 export const apiJava = axios.create({
-  baseURL: "mecnetapi-production.up.railway.app",
+  baseURL: "http://mecnetapi-production.up.railway.app",
 });
