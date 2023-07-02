d6255b24a1effd7575eca0d1b13fa937e35aca23
Eduardo Araujo <eduardoaraujo.jf@gmail.com>
2023-07-02 19:26:55 -0300

    Finish: MECNETv1

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,9 +1,11 @@
 export function auxDate(dataString: string | undefined) {
   if (dataString === undefined) return;
-  const data = dataString.split("T");
+  const dataAux = new Date(dataString);
+  dataAux.setUTCHours(dataAux.getUTCHours() - 6);
+  const data = dataAux.toJSON().split("T");
   const hour = data[1].split(":");
   const date = data[0].split("-");
-  return `${date[2]}/${date[1]}/${date[0]} | ${hour[0]}:${hour[1]}h  `;
+  return `${date[2]}/${date[1]}/${date[0]} | ${hour[0]}:${hour[1]}h`;
 }
 
 export function auxPrice(price?: number) {
